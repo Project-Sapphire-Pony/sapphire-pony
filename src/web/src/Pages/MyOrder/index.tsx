5a959078bfd3f261cf792b1b1477183200e9ca2d
Katherine Wen <wen.535@osu.edu>
2024-03-18 10:03:33 -0400

    Add Pages

---
+++ src/web/src/Pages/MyOrder/index.tsx
@@ -0,0 +1,10 @@
+const MyOrder: React.FC = () => {
+    return (
+        <div>
+            <h1>MyOrder</h1>
+            {/*Add your content here */}
+        </div>
+    );
+};
+
+export default MyOrder;
---
+++ src/web/src/Pages/MyOrders/index.tsx
@@ -0,0 +1,10 @@
+const MyOrders: React.FC = () => {
+    return (
+        <div>
+            <h1>MyOrders</h1>
+            {/*Add your content here */}
+        </div>
+    );
+};
+
+export default MyOrders;
