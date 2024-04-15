131bbc4ef318c72a32ed9e2eca63a1bb4b595525
Katherine Wen <wen.535@osu.edu>
2024-04-15 13:16:26 -0400

    app tests - more advanced

--- src/web/src/App.test.tsx
+++ src/web/src/App.test.tsx
@@ -13,7 +13,15 @@ describe("App component tests", () => {
     test("should show 'All' link in the menu", () => {
         render(<App/>)
 
-        expect(screen.getByText(/All/i))
+        expect(screen.getByRole('link', {name: /All/i}))
+        .toBeDefined()
+    })
+
+    test("should show 'Home' heading in 'H1' tag", () => {
+
+        render(<App/>)
+
+        expect(screen.getByRole('heading', {level: 1, name: /Home/i}))
         .toBeDefined()
     })
 
