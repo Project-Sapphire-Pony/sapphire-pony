aab3689a7d4f5f406da06f620ca21e3936d22717
Katherine Wen <wen.535@osu.edu>
2024-04-15 11:52:59 -0400

    simple tests for app component

---
+++ src/web/src/App.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe("App component tests", () => {
+    test("should show title all the time", () => {
+        
+        render(<App/>)
+        
+        expect(screen.getByText(/Sapphire-Pony/i))
+        .toBeDefined()
+    })
+
+    test("should show 'All' link in the menu", () => {
+        render(<App/>)
+
+        expect(screen.getByText(/All/i))
+        .toBeDefined()
+    })
+
+   
+})
