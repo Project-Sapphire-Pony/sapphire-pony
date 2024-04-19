aab4d5dabe29c329b99271afe2e49d1a58681f0a
Katherine Wen <wen.535@osu.edu>
2024-04-19 08:09:01 -0400

    update

--- src/api/SAPPHIRE.PONY.Data/obj/Debug/net8.0/SAPPHIRE.PONY.Data.AssemblyInfo.cs
+++ src/api/SAPPHIRE.PONY.Data/obj/Debug/net8.0/SAPPHIRE.PONY.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAPPHIRE.PONY.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30d3195a54b290709be34e0f9050c848b67f7ad3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbb722143fa8f20397a30ea2630881409d1e08e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAPPHIRE.PONY.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAPPHIRE.PONY.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
