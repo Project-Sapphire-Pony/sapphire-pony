b535f6af3cf94e7aa3e2ba988705f43e170d5875
Katherine Wen <wen.535@osu.edu>
2024-04-23 12:23:39 -0400

    api update

--- src/api/SAPPHIRE.PONY.Domain/obj/Debug/net8.0/SAPPHIRE.PONY.Domain.AssemblyInfo.cs
+++ src/api/SAPPHIRE.PONY.Domain/obj/Debug/net8.0/SAPPHIRE.PONY.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SAPPHIRE.PONY.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aab4d5dabe29c329b99271afe2e49d1a58681f0a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62951eb2b0bcdefc9dc38336095ad11ef0e3b5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SAPPHIRE.PONY.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SAPPHIRE.PONY.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
